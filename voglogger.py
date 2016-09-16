5639dc6af0254b2ea1a676de70a9f11551d16d1b
njyjn <njyjustin@yahoo.com>
2016-09-16 17:59:05 +0800

    Imported darrenwee's voglogger file.

---
+++ voglogger.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+"""
+	logger management for VOGLbot
+	writes out to both the console and a file 'voglbot.log'
+"""
+
+import sys
+import logging
+import time
+
+logging.basicConfig(
+	filename = 'voglbot.log',
+	filemode = 'w',
+	level=logging.DEBUG,
+	format='%(asctime)s: %(message)s',
+	datefmt = '%d-%m %H:%M:%S',
+)
+# for console logging
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)-12s : %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+
+logger = logging.getLogger()
+logging.getLogger('').addHandler(console)
