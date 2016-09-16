2702ca67ef8da7845503e069a15371b0387ec1ce
njyjn <njyjustin@yahoo.com>
2016-09-16 18:25:55 +0800

    Performed DB setup.

--- manager.py
+++ manager.py
@@ -22,28 +22,27 @@ except pymongo.errors.ConnectionFailure as e:
 db = connection['ariadne']
 
 # collection
-students = db['students']
-houses = db['houses']
+cgls = db['cgls']
+cgs = db['cgs']
 
 ### helper functions ###
-def houseIsValid(house):
-    houses = {
-        'verde': True,
-        'nox': True,
-        'aether': True,
-        'hydra': True,
-        'angstrom': True,
-        'sigvar': True,
+def cgIsValid(cg):
+    cg = {
+        'mj': True,
+        'tpja': True,
+        'tpjb': True,
+        'vja': True,
+        'vjb': True,
+        'tj': True,
+        'dmh': True,
         'all': True,
     }
-    return houses.get(house, False)
+    return cgs.get(cg, False)
 
 def fieldIsValid(field):
     fields = {
         'name': True,
-        'type': True,
-        'house': True,
-        'color': True,
+        'cg': True,
         'chatID': True,
     }
     return fields.get(field, False)
@@ -59,10 +58,8 @@ def enumerator(cursor, fields):
     for person in cursor:
         reply += str(i) + '.\n'
         for field in fields:
-            if field == 'house':
+            if field == 'cg':
                 reply += '%s: %s\n' % (field.title(), person[field][0].title())
-            elif field == 'diet' or field == 'medical':
-                reply += '%s: %s\n' % (field.title(), person[field])
             else:
                 reply += '%s: %s\n' % (field.title(), person[field].title())
         i += 1
@@ -74,104 +71,75 @@ def makeTimestamp():
     return datetime.datetime.fromtimestamp(time.time()).strftime('%I:%M%p, %d %B')
 
 def exists(chatID):
-    if students.find_one({'chatID': chatID}) == None:
+    if cgls.find_one({'chatID': chatID}) == None:
         return False
     return True
-
-def yell(bot, audience, message, requester):
-    typeList = ['all', 'cvogl', 'ogl', 'freshie', 'gov']
-    if audience in typeList:
-        # yell to types
-
-        if students.find( {'type': audience} ) != None:
-            recipients = students.find( {'type': audience} )
-
-            for recipient in recipients:
-                # send to each recipient
-                bot.sendMessage(recipient['chatID'], message)
-        else:
-            # no records from audience found
-            bot.sendMessage(requester, 'Yell failed: no records found!')
-    elif houseIsValid(audience):
-        # yell to house (excluding 'all')
-
-        if students.find( {'color': audience} ) != None:
-            recipients = students.find( {'color': audience} )
-
-            for recipient in recipients:
-                # send to each recipient
-                bot.sendMessage(recipient['chatID'], message)
-        else:
-            # no records from audience found
-            bot.sendMessage(requester, 'Yell failed: no records found!')
 
 ### executive functions ###
 
 # /add
-def add(house, name, chatID):
+def add(cg, name, chatID):
 
-    if houseIsValid(house):
+    if cgIsValid(cg):
 
         # check for duplicate name
-        if students.find( {'name': name, 'house': house }).count() != 0:
+        if cgls.find( {'name': name, 'cg': cg }).count() != 0:
             logger.warn('/add failed due to duplicate entry')
-            return 'There is already someone with that name and house in the database. /add failed.'
+            return 'There is already someone with that name and cg in the database. /add failed.'
 
         timestamp = str(datetime.datetime.now())
     
-        logger.info('Adding \'%s\' from \'%s\' of id %s' % (name, house, chatID))
+        logger.info('Adding \'%s\' from \'%s\' of id %s' % (name, cg, chatID))
         
-        student = {
+        cgl = {
             'name': name,
-            'type': 'freshie',
-            'house': [house, 'all'],
-            'color': house,
+            'cg': [cg, 'all'],
             'chatID': chatID
         }
 
-        students.insert_one(student)
-        logger.info('Added \'%s\' of id %d to \'%s\' house' % (chatID, name, house))
+        cgls.insert_one(cgl)
+        logger.info('Added \'%s\' of id %d to \'%s\' cg' % (chatID, name, cg))
         return
     logger.info('/add query failed (invalid parameters)')
     return
 
 # /remove
-def remove(house, name, requester):
-    reply = 'Removed \'%s\' of \'%s\' house from database.' % (name, house)
+def remove(cg, name, requester):
+    reply = 'Removed \'%s\' of \'%s\' cg from database.' % (name, cg)
 
     # perform remove
-    if students.find( {'name': name, 'house': house }).count() == 0:
+    if cgls.find( {'name': name, 'cg': cg }).count() == 0:
         return 'No such record found.'
-    elif students.find( {'name': name, 'house': house }).count() == 1:
-        students.remove( {'name': name, 'house': house}, 1 )
+    elif cgls.find( {'name': name, 'cg': cg }).count() == 1:
+        cgls.remove( {'name': name, 'cg': cg}, 1 )
         logger.info(whoIs(requester) + ': ' + reply)
         return reply
 
-    return 'Multiple records with the same name and house found. Contact Justin for removal.'
+    return 'Multiple records with the same name and cg found. Contact Justin for removal.'
 
 # /enum
-def getEnumerate(house, requester):
-    if houseIsValid(house):
+def getEnumerate(cg, requester):
+    if cgIsValid(cg):
 
-        # cover the all case and single house case
-        if house == 'all':
-            houses = ['nox', 'hydra', 'verde', 'sigvar', 'aether', 'angstrom']
+        # cover the all case and single cg case
+        if cg == 'all':
+            cgs = ['mj', 'vja', 'vjb', 'tpja', 'tpjb', 'tj', 'dmh']
         else:
-            houses = [house]
+            cgs = [cg]
 
         reply = ''
-        for target_house in houses:
+        for target_cg in cgs:
             reply += '=====================\n'
-            reply += target_house.upper() + '\n\n'
+            reply += target_cg.upper() + '\n\n'
             # query for database cursor
-            results = students.find( {'house': target_house} )
+            results = cgls.find( {'cg': target_cg} )
             
             # sort results
-            results.sort( [ ('color', 1), ('name', 1) ] )
+            results.sort( [('name', 1) ] )
     
             reply += status.title() + '\n'
     
-            # catch empty house/mode query
+            # catch empty cg/mode query
             if results.count() == 0:
                 reply += 'No records found.\n'
             else:
@@ -186,50 +154,50 @@ def getEnumerate(house, requester):
     else:
         # catch invalid parameters
         logger.info('%s: /enum query failed (invalid parameters)' % whoIs(requester))
-        return 'Invalid house or status. See \'/help enumerate\''
+        return 'Invalid cg or status. See \'/help enumerate\''
 
-    logger.info('%s: Returning enumeration for \'%s\' in \'%s\'' % (whoIs(requester), status, house))
+    logger.info('%s: Returning enumeration for \'%s\' in \'%s\'' % (whoIs(requester), status, cg))
     return reply
 
 # /find
-def find(house, pattern, requester):
+def find(cg, pattern, requester):
     reply = ''
 
-    if houseIsValid(house):
+    if cgIsValid(cg):
         # query for database cursor
-        results = students.find( {'name': { '$regex': '.*' + pattern + '.*'}, 'house': house } )
+        results = cgls.find( {'name': { '$regex': '.*' + pattern + '.*'}, 'cg': cg } )
     
         # sort results
-        results.sort( [ ('color', -1), ('name', 1) ] )
+        results.sort( [ (('name', 1) ] )
 
-        details = ['name', 'color', 'type', 'chatID']
+        details = ['name', 'chatID']
     
         # build the reply
-        reply += 'Finding any names containing \'%s\' for \'%s\'\n\n' % (pattern, house)
+        reply += 'Finding any names containing \'%s\' for \'%s\'\n\n' % (pattern, cg)
         reply += enumerator(results, details)
     else:
         # catch shitty parameters
         logger.info('%s: /find query failed (invalid parameters)' % whoIs(requester))
-        return 'Invalid house. See \'/help find\''
+        return 'Invalid cg. See \'/help find\''
 
-    logger.info('%s: Returning find query for pattern \'%s\' for \'%s\'' % (whoIs(requester), pattern, house))
+    logger.info('%s: Returning find query for pattern \'%s\' for \'%s\'' % (whoIs(requester), pattern, cg))
     return reply
 
 # /updater
-def updater(house, name, field, content, requester):
-    reply = 'Updating \'%s\' for \'%s\' in \'%s\' house.\n\n' % (field, name, house)
+def updater(cg, name, field, content, requester):
+    reply = 'Updating \'%s\' for \'%s\' in \'%s\' cg.\n\n' % (field, name, cg)
 
-    if houseIsValid(house):
+    if cgIsValid(cg):
         # check if result was found
-        if students.find_one( {'name': name, 'house':house} ) == None:
+        if cgls.find_one( {'name': name, 'cg':cg} ) == None:
             # no results
-            reply += 'Could not find \'%s\' from \'%s\' house.' % (name, house)
+            reply += 'Could not find \'%s\' from \'%s\' cg.' % (name, cg)
         else:
             # got results
-            if fieldIsValid(field) and field != 'house':
-                logger.info('%s: Updating \'%s\' for \'%s\' in \'%s\' with: \'%s\'' % (whoIs(requester), field, name, house, content))
-                students.update_one( {'name': name, 'house': house}, { '$set': { field: content } } ) # perform update
-                reply += 'Successfully updated \'%s\' field for \'%s\' in \'%s\' with: \'%s\'' % (field, name, house, content)
+            if fieldIsValid(field) and field != 'cg':
+                logger.info('%s: Updating \'%s\' for \'%s\' in \'%s\' with: \'%s\'' % (whoIs(requester), field, name, cg, content))
+                cgls.update_one( {'name': name, 'cg': cg}, { '$set': { field: content } } ) # perform update
+                reply += 'Successfully updated \'%s\' field for \'%s\' in \'%s\' with: \'%s\'' % (field, name, cg, content)
             else:
                 reply += 'Invalid field.'
     return reply
