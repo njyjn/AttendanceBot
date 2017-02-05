4d2db062c1401ff6b615413cfab770fcecc43818
njyjn <njyjustin@yahoo.com>
2017-02-05 16:21:28 +0800

    Do nothing if user can't receive message from broadcast.

--- broadcaster.py
+++ broadcaster.py
@@ -4,4 +4,7 @@ def yell(bot, audience, message, requester):
     
     recipients = manager.prepareYell(audience)
     for recipient in recipients:
-        bot.sendMessage(recipient['chatID'], message)
+        try:
+        	bot.sendMessage(recipient['chatID'], message)
+        except:
+        	pass
